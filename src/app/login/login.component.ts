3d4558e61ae696de45e199085f067234a2ce8f07
小小福 <13183156452@163.com>
2019-05-31 15:17:55 +0800

    注释

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -52,7 +52,8 @@ export class LoginComponent implements OnInit {
 
     // })
      this.router.navigate(['home'])
-          // this.nav.navigateRoot(['home']);
+     //设置home为跟目录
+    // this.nav.navigateRoot(['home']);
   }
 
   findPassword() {
